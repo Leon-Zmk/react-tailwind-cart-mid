2d15666c63686b667f03d3ed1818271617d679c9
Leon <leonzmkdev@gmail.com>
2023-07-12 00:44:00 +0630

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Products from './components/Products'
 
-function App({products}) {
-  console.log(products)
+function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Products />} />
+      </Routes>
     </div>
   )
 }
