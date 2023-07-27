e397a336e34ed682e28466f0e7798bbf34af754e
Leon <leonzmkdev@gmail.com>
2023-07-27 09:58:09 +0630

    empty-view

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -3,6 +3,8 @@ import { StatContextCustom } from '../contexts/StatContext';
 import {IoIosArrowUp,IoIosArrowDown} from 'react-icons/io';
 import Cart from './Cart';
 import { useEffect } from 'react';
+import { AiFillShop, AiFillShopping } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 function AddToCart() {
 
@@ -21,21 +23,30 @@ function AddToCart() {
     }
     const Total = carts?.reduce((pv,iv) => pv + iv.price,0);
   return (
-    <div>
-        <div className='flex flex-col mt-20'>
-        {carts?.map(cart => {
-            return(
-                <Cart key={cart.id} item={cart} increaseTotal={increaseTotal} decreaseTotal={decreaseTotal}></Cart>
-            )
-        })}
+        
+     carts.length > 1 ? (
+        <div>
+            <div className='flex flex-col mt-20'>
+            {carts?.map(cart => {
+                return(
+                    <Cart key={cart.id} item={cart} increaseTotal={increaseTotal} decreaseTotal={decreaseTotal}></Cart>
+                )
+            })}
+        </div>
+        <hr className=" w-[60%] mx-auto" />
+        <div className=' flex justify-around mb-20' >
+            <p>Total</p>
+            <p>${mainTotal.toFixed(2)}</p>
+        </div>
     </div>
-    <hr className=" w-[60%] mx-auto" />
-    <div className=' flex justify-around mb-20' >
-        <p>Total</p>
-        <p>${mainTotal.toFixed(2)}</p>
+    ) : 
+    <div className='flex flex-col items-center'>
+        <p className=' text-zinc-700 text-2xl p-5 text-center'>There Is No Cart</p>
+        <Link to="/" className='p-2 rounded bg-cyan-500'>
+          <AiFillShopping className=' text-black text-2xl rounded'/>
+        </Link>
+
     </div>
-    </div>
-   
 
   )
 }
